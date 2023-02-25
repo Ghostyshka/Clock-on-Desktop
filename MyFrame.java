aa49dae419d24e99d73d80d2c7537862fbd42bf7
Ghostyhka <denis2018steam@gmail.com>
2023-02-25 15:34:08 +0200

    fix size bug add more realizations for Form and design
    click every second

--- MyFrame.java
+++ MyFrame.java
@@ -13,17 +13,31 @@ public class MyFrame extends JFrame {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("Clock");
         this.setLayout(new FlowLayout());
-        this.setSize(300, 200);
+        this.setSize(400, 250);
         this.setResizable(false);
 
         timeFormat = new SimpleDateFormat("hh:mm:ss a");
 
         timeLabel = new JLabel();
+        timeLabel.setFont(new Font("Verdana", Font.PLAIN,50));
+        timeLabel.setForeground(Color.blue);
+        timeLabel.setBackground( Color.orange);
+        timeLabel.setOpaque(true);
 
-        time = timeFormat.format(Calendar.getInstance().getTime());
-        timeLabel.setText(time);
-        
         this.add(timeLabel);
         this.setVisible(true);
+
+        setTime();
+    }
+    public void setTime(){
+        while (true) {
+            time = timeFormat.format(Calendar.getInstance().getTime());
+            timeLabel.setText(time);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
